58ac1627eb2dbc2d1fb598a36bac0b6cd63bbc3b
Malani Snowden <109847401+cxnejxblancx@users.noreply.github.com>
2024-01-06 20:25:53 -0500

    Q3

--- Homework/Hw2/hw2_q3.py
+++ Homework/Hw2/hw2_q3.py
@@ -1,12 +1,24 @@
-# import
-from math import sin
+# constants
+MIN_IN_HOUR = 60
+MIN_IN_DAY = 60 * 24
 
-# input
-frequency = float(input("Enter a value for the frequency, w: "))
-duration = float(input("Enter a value for the duration of the sound wave, T: "))
+# inputs
+semi_days = int(input("Please enter the number of days Semi has worked: "))
+semi_hours = int(input("Please enter the number of hours Semi has worked: "))
+semi_minutes = int(input("Please enter the number of minutes Semi has worked: "))
+daniel_days = int(input("Please enter the number of days Daniel has worked: "))
+daniel_hours = int(input("Please enter the number of hours Daniel has worked: "))
+daniel_minutes = int(input("Please enter the number of minutes Daniel has worked: "))
+
+# conversions
+semi_daniel_days_in_min = ((semi_days + daniel_days) * (60 * 24))
+semi_daniel_hours_in_min = ((semi_hours + daniel_hours) * (60))
+semi_daniel_minutes = ((semi_daniel_days_in_min) + (semi_daniel_hours_in_min) + (semi_minutes) + (daniel_minutes))
 
 # calculations
-amp_spectrum = round(((2 * sin(frequency * duration)) / frequency), 3)
+days_combined = (semi_daniel_minutes // (min_in_day))
+hours_combined = ((semi_daniel_minutes % (min_in_day)) // (min_in_hour))
+min_combined = ((semi_daniel_minutes % (min_in_day)) % (min_in_hour))
 
 # final print
-print("The amplitude spectrum of this Fourier transform is: " + str(amp_spectrum))
+print("The total time both of them worked together is: " + str(days_combined) + " days, " + str(hours_combined) + " hours and " + str(min_combined) + " minutes")
