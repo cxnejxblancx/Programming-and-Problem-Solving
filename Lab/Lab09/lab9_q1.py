9baf335b3d2df2895828f99f356851f5ff7a76d9
Malani Snowden <109847401+cxnejxblancx@users.noreply.github.com>
2024-01-07 16:02:23 -0500

    Q1

--- Lab/Lab09/lab9_q1.py
+++ Lab/Lab09/lab9_q1.py
@@ -1,1 +1,37 @@
+# a
+    # words.txt does exist
+"""
+>>> File cannot be found
+>>> Val: 15
+>>> File cannot be found
+>>> Val: 30
+>>> File cannot be found
+>>> Val: 45
+>>> File cannot be found
+>>> Val: 60
+>>> File cannot be found
+>>> Val: 75
+"""
 
+    # words.txt does exist
+"""
+>>> Val: 10
+>>> Val: 20
+>>> Val: 30
+>>> Val: 40
+>>> Val: 50
+"""
+
+
+# b 
+"""
+# 1 * 3
+>>> 3
+# 2 * 4
+>>> 8
+# 3 * 1
+>>> 3
+# 4 * 2
+>>> 8
+>>> True
+"""
