8d4606d5f70d9a1f2e91a561028ab04e96b39cfe
Malani Snowden <109847401+cxnejxblancx@users.noreply.github.com>
2024-01-07 15:58:40 -0500

    Q1

--- Lab/Lab08/lab8_q1.py
+++ Lab/Lab08/lab8_q1.py
@@ -1,1 +1,15 @@
+# PART 1
+#   >> hhhhheeeeellllllllllooooo
 
+# PART 2
+#   >> 10
+#   >> whoop
+#   >> 15
+#   >> 10
+#   >> whoop
+#   >> 18
+#   >> 20
+
+"""
+When you return something, you're saying that that function call is equal to that value
+"""
