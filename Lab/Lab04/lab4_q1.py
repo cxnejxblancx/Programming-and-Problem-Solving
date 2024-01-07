916272268305ecb9a13052f7b100007cfeaf5ff2
Malani Snowden <109847401+cxnejxblancx@users.noreply.github.com>
2024-01-07 15:48:19 -0500

    Q1

--- Lab/Lab04/lab4_q1.py
+++ Lab/Lab04/lab4_q1.py
@@ -1,1 +1,62 @@
+# default for sep is " " (space)
+    # which is why the common a string prints a space
+# chang sep at the end of the string (, sep = "c")
 
+# print statements in Python end with a newline (\n)
+# this can be changed with the "end" parameter
+    # x = 5
+    # y = 6
+    # print (x)
+    # print (y)
+    # >>> 5
+    # >>> 6
+    # print(x, end = " ")
+    # print(y)
+    # >>> 5 6
+
+# a)
+# >>> 0
+# >>> 1
+# >>> 2
+# >>> 3
+# >>> 4
+# >>> 5
+# >>> 6
+# >>> 7
+# >>> 8
+# >>> 9
+
+# b) 
+# >>> 1
+# >>> 3
+# >>> 5
+# >>> 7
+# >>> 9
+# >>> 11
+
+# c)
+# >>> 43
+# >>> 40
+# >>> 37
+# >>> 34
+
+# d)
+# >>> 1
+# >>> 3
+# >>> 9
+# >>> 27
+
+# e)
+# >>> fizz
+# >>> 14
+# >>> fizz
+# >>> 16
+# >>> fizz
+# >>> 18
+# >>> fizz
+# >>> 20
+# >>> fizz
+# >>> 22
+
+print("Hello", "World", sep = "", end = "*")
+# HelloWorld*
