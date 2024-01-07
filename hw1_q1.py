69711f09fe69b8502a79b6e7250b2ea35d7a84cf
Malani Snowden <109847401+cxnejxblancx@users.noreply.github.com>
2024-01-06 19:50:16 -0500

    Delete hw1_q1.py

--- hw1_q1.py
+++
@@ -1,6 +0,0 @@
-# input
-user_name = input("Please enter your name: ")
-user_age = int(input("Please enter your age: "))
-
-# print
-print(user_name + ", " + str(user_age) + ", is taking CS-UY 1114.")
